952f28a30551b7ad24b873e39b7a64fae458fad9
Danilo Chang <ray2501@gmail.com>
2019-10-14 16:24:51 +0800

    Update package

--- drawing/Program.cs
+++ drawing/Program.cs
@@ -7,17 +7,14 @@ namespace drawing
     {
         static void Main(string[] args)
         {
-            Image image = new Bitmap(1024, 768);
-            Graphics graph = Graphics.FromImage(image);
-            graph.Clear(Color.Azure);
+           Image image = new Bitmap(1024, 768);
+           Graphics graph = Graphics.FromImage(image);
+           graph.Clear(Color.Azure);
 
-            Pen pen = new Pen(Brushes.Black);
-            graph.DrawLines(pen, new Point[] { new Point(10, 10), new Point(800, 600) });
-            graph.DrawString("Hello drawing from .NET Core :)",
-            new Font(new FontFamily("DecoType Thuluth"), 20, FontStyle.Bold),
-            Brushes.Blue, new PointF(150, 90));
+           Pen pen = new Pen(Brushes.Black);
+           graph.DrawLines(pen, new Point[] { new Point(10,10), new Point(800, 600) });
 
-            image.Save("graph.jpeg", System.Drawing.Imaging.ImageFormat.Png);
+           image.Save("graph.jpeg", System.Drawing.Imaging.ImageFormat.Png);
         }
     }
 }
